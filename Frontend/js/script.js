14e2f8c9a0c1409d30859ddf8be6a960b520fd61
Ramya Venkatesh <rajaramya559@gmail.com>
2024-01-21 19:19:54 +0530

    Update add task fucntionality

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -6,10 +6,27 @@ const addTask = () => {
     if (!taskDescription) {
         alert("Please enter your works to be done");
     }
+    else if (taskDescription.length > 30) {
+        alert("Please enter your works to be done in 30 characters");
+        inputBox.value = '';
+    }
     else {
         let listElement = document.createElement('li');
         listElement.innerHTML = taskDescription;
         listContainer.appendChild(listElement);
         inputBox.value = '';
+        let span = document.createElement('span');
+        span.setAttribute('class', 'modify-icons');
+        let editIcon = document.createElement('img');
+        let removeIcon = document.createElement('img');
+        editIcon.setAttribute('src', './images/edit.svg');
+        editIcon.setAttribute('alt', 'edit');
+        editIcon.setAttribute('class', 'icon-resize');
+        removeIcon.setAttribute('src', './images/remove.svg');
+        removeIcon.setAttribute('alt', 'remove');
+        removeIcon.setAttribute('class', 'icon-resize');
+        span.appendChild(editIcon);
+        span.appendChild(removeIcon);
+        listElement.appendChild(span);
     }
 }
