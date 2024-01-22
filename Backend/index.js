c27e4ce3f7f072b9a16380d920290b90eca2f485
Ramya Venkatesh <rajaramya559@gmail.com>
2024-01-22 09:35:04 +0530

    Add remove task

--- Backend/index.js
+++ Backend/index.js
@@ -39,7 +39,7 @@ app.post('/createTask', async (req, res) => {
 
 app.get('/getTasks', async (req, res) => {
     try {
-        let response = await taskModel.find({});
+        let response = await taskModel.find({ isActive: true });
         res.status(200).send({ data: response || [] })
     } catch (e) {
         res.status(500).send({ error: e })
@@ -60,7 +60,28 @@ app.put('/updateTask/:type', async (req, res) => {
         else {
             res.status(400).send({ error: isvalid.message })
         }
-    } 
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).send({ error: e })
+
+    }
+})
+
+app.put('/removeTask', async (req, res) => {
+    try {
+        isvalid = validation('remove', req);
+        console.log(isvalid)
+        if (!isvalid.status) {
+            let response = await taskModel.findByIdAndUpdate(req.body.id, req.body);
+            (!response) ?
+                res.status(404).send({ message: 'Task is not found' }) :
+                res.status(200).send({ message: 'Task removed successfully' })
+        }
+        else {
+            res.status(400).send({ error: isvalid.message })
+        }
+    }
     catch (e) {
         console.log(e)
         res.status(500).send({ error: e })
@@ -74,19 +95,35 @@ const validation = (validationType, req) => {
         message: []
     }
     let errorMessage = [];
-    if (validationType == 'update') {
-        let { type } = req.params;
-        console.log(req.params)
-        if (type == 'info' || 'status') {
-            if (!req.body.id) {
-                errorMessage.push('Id is a required field')
+    let updateType = req.params.type
+    if (validationType == 'create' || (validationType == 'update' && updateType == 'info')) {
+        if (!req.body.description) {
+            errorMessage.push('Description is a required field')
+            response = {
+                status: true,
+                message: errorMessage
+            }
+        }
+        else {
+            if (typeof req.body.description != 'string') {
+                errorMessage.push('Description is a string type')
                 response = {
                     status: true,
                     message: errorMessage
                 }
             }
         }
-        if (type == 'status') {
+    }
+    if (validationType == 'update' || 'remove') {
+
+        if (!req.body.id) {
+            errorMessage.push('Id is a required field')
+            response = {
+                status: true,
+                message: errorMessage
+            }
+        }
+        if (updateType == 'status') {
             if (!req.body.status) {
                 errorMessage.push('Status is a required field')
                 response = {
@@ -95,9 +132,27 @@ const validation = (validationType, req) => {
                 }
             }
             else {
-                console.log("test")
-                if (typeof req.body.status == 'number') {
+                if (typeof req.body.status != 'string') {
                     errorMessage.push('Status is a string type')
+                    response = {
+                        status: true,
+                        message: errorMessage
+                    }
+                }
+            }
+        }
+        if (updateType == 'remove') {
+            console.log(req.body.isActive)
+            if (req.body.isActive == (null || undefined)) {
+                errorMessage.push('isActive is a required field')
+                response = {
+                    status: true,
+                    message: errorMessage
+                }
+            }
+            else {
+                if (typeof req.body.isActive != 'boolean') {
+                    errorMessage.push('isActive is a boolean type')
                     response = {
                         status: true,
                         message: errorMessage
@@ -107,23 +162,6 @@ const validation = (validationType, req) => {
         }
 
     }
-    if (validationType == 'create' || 'update') {
-        if (!req.body.description) {
-            errorMessage.push('Description is a required field')
-            response = {
-                status: true,
-                message: errorMessage
-            }
-        }
-        else {
-            if (typeof req.body.description == 'number') {
-                errorMessage.push('Description is a string type')
-                response = {
-                    status: true,
-                    message: errorMessage
-                }
-            }
-        }
-    }
+
     return response
 }
